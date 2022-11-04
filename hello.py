c85d28ba448cc67f89d65684df4dbc63147dcd1b
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-04 21:32:27 +0000

    repititive lines

--- hello.py
+++ hello.py
@@ -2,7 +2,14 @@ h = "hello world"
 
 # testing manual git add ., git commit, git push
 
-for x in range(10):
+for x in range(1):
+    print(h)
+
+print("My name is Bond, James Bond")
+
+print("My name is Niki Minaaaj")
+
+for x in range(1):
     print(h)
 
 print("My name is Bond, James Bond")
