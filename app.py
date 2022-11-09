3b3b5e57bd9646b53ced0415f4053cafe0886d0d
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-09 02:53:00 +0000

    Wed, Nov 9, 2022, 2:53 AM +00:00

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, session, render_template, request, redirect
-
 app = Flask(__name__)
 
 @app.route("/")
 def route():
-    return render_template("test.html")
+    name = "Ammad"
+    return render_template("test.html", name = name)
