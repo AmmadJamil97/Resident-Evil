100881b0f8f1699f91d35c9ec68cdcfdd3a0e2a8
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-10 14:19:12 +0000

    Thu, Nov 10, 2022, 2:19 PM +00:00

--- templates/app.py
+++ templates/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, request, redirect
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/test3.html")
 def route():
     return render_template("test3.html")
