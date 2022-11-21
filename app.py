956dc8c16d681157a30ecaf780118baea18ad59a
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-21 22:14:10 +0000

    Mon, Nov 21, 2022, 10:14 PM +00:00

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ def route():
     return render_template("test4.html")
 
 
+
+
+# Create an instance of tkinter frame or window
+win = Tk()
+
+# Set the size of the tkinter window
+win.geometry("700x350")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
 
@@ -22,4 +31,13 @@ def register():
         if not request.form.get("username"):
             messagebox.showerror("nousername", "Please insert username")
 
+# Create a label widget
+label = Label(win, text="Click the button to show the message ",
+font=('Calibri 15 bold'))
+label.pack(pady=20)
 
+# Create a button to delete the button
+b = Button(win, text="Click Me", command=on_click)
+b.pack(pady=20)
+
+win.mainloop()
