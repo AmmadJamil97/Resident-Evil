7ceab07f6e3c8cae7d9ac8f19e99a94fc5746ee9
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-21 22:31:09 +0000

    Mon, Nov 21, 2022, 10:31 PM +00:00

--- app.py
+++ app.py
@@ -16,6 +16,6 @@ def register():
 
         # Ensure user submit a username
         if not request.form.get("username"):
-            return render_template("must provide username")
+            return render_ template("must provide username")
 
 
