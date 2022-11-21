c9703319912424c701aa940b83f9ea3e075f5943
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-21 22:15:18 +0000

    Mon, Nov 21, 2022, 10:15 PM +00:00

--- app.py
+++ app.py
@@ -1,22 +1,9 @@
 from flask import Flask, session, render_template, request, redirect
-from tkinter import *
-from tkinter import messagebox
-
-
 app = Flask(__name__)
 
 @app.route("/")
 def route():
     return render_template("test4.html")
-
-
-
-
-# Create an instance of tkinter frame or window
-win = Tk()
-
-# Set the size of the tkinter window
-win.geometry("700x350")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -29,15 +16,6 @@ def register():
 
         # Ensure user submit a username
         if not request.form.get("username"):
-            messagebox.showerror("nousername", "Please insert username")
+            showerror("nousername", "Please insert username")
 
-# Create a label widget
-label = Label(win, text="Click the button to show the message ",
-font=('Calibri 15 bold'))
-label.pack(pady=20)
 
-# Create a button to delete the button
-b = Button(win, text="Click Me", command=on_click)
-b.pack(pady=20)
-
-win.mainloop()
