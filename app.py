89e6fe37f55f6d6d711b0018f750435508d0dcc0
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-22 14:25:37 +0000

    Tue, Nov 22, 2022, 2:25 PM +00:00

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash
 from cs50 import SQL
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
