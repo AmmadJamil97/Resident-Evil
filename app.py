4b1c6343cabe94e4d240ac7e1c01e8629cf396de
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-22 20:31:02 +0000

    Tue, Nov 22, 2022, 8:31 PM +00:00

--- app.py
+++ app.py
@@ -40,8 +40,11 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # check for WRONG username exist in 2nd row (1st row is title) + OR WRONG password for that username
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("passowrd")):
-            return render_template("fail.html", value = "Invalid username and/or password")
+        if len(rows) != 1:
+            return render_template("fail.html", value = "Invalid username")
+
+        elif not check_password_hash(rows[0]["hash"], request.form.get("passowrd")):
+            return render_template("fail.html", value = "Invalid password")
 
         return render_template("success.html")
 
