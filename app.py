e8e3b953b6ec127aee49cf71c1ec090348df8bc3
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-27 03:54:57 +0000

    Sun, Nov 27, 2022, 3:54 AM +00:00

--- app.py
+++ app.py
@@ -21,35 +21,6 @@ db = SQL("sqlite:///finalproject.db")
 def default():
     return render_template("test4.html")
 
-def chatroom():
-    #return render_template("login.html")
-
-    rows = db.execute("SELECT username, messages FROM users WHERE username =?", session["user_id"])
-
-    for row in rows:
-        row["username"]
-        row["messages"]
-
-    return render_template("chatroom.html",rows=rows)
-
-@app.route("/message", methods=["GET", "POST"])
-def message():
-
-    if request.method == "POST":
-
-        rows = db.execute("SELECT * from users WHERE id = ?", session["user_id"])
-
-        for row in rows:
-            db.execute("INSERT INTO users (message) VALUES(?);", request.form.get("message"))
-
-        for row in rows:
-            row["username"]
-            row["messages"]
-
-
-        return render_template("chatroom.html", rows = rows)
-    else:
-        return render_template("test4.html")
 
 @app.route("/test4")
 def index():
