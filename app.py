4bf423f08d045ae9d7f4ce759901a3721b0ec94c
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-28 03:30:21 +0000

    Mon, Nov 28, 2022, 3:30 AM +00:00

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def message():
 
         # rows = db.execute("SELECT * from")
         for row in rows:
-            row["username"] = db.execute("SELECT username FROM users WHERE id =?;", session["user_id"])
+            row["username"] = db.execute("SELECT username FROM users;")
             row["message"] = db.execute("SELECT message FROM message WHERE user_id =?;", session["user_id"])
 
 
