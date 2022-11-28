ae63f1d6ffc2b4f0270b2276e43a33c6139b1da2
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-28 02:38:50 +0000

    Mon, Nov 28, 2022, 2:38 AM +00:00

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def message():
         rows = db.execute("SELECT * FROM users WHERE username =?;", session["user_id"])
 
         # Insert into the id's respective message box to INSERT the message
-        db.execute("INSERT INTO users (messages) VALUES(?);", request.form.get("message"))
+        db.execute("INSERT INTO users (messages) VALUES('?');", request.form.get("message"))
 
         # rows = db.execute("SELECT * from")
         for row in rows:
