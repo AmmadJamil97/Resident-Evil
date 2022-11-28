22da0f5880df50362afe82bdbbcb1520f533a26d
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-28 03:33:22 +0000

    Mon, Nov 28, 2022, 3:33 AM +00:00

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def message():
         # rows = db.execute("SELECT * from")
         for row in rows:
             row["username"] = db.execute("SELECT username FROM users;")
-            row["message"] = db.execute("SELECT message FROM message WHERE user_id =?;", session["user_id"])
+            row["message"] = db.execute("SELECT message FROM message WHERE user_id =?;", ("SELECT id FROM users;"))
 
 
         # # connect loop in HTML and PY
