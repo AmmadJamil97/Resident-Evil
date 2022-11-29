8ba8b4c8bc13b3489ba08047ec9253099ccb13dc
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-11-29 01:36:40 +0000

    Tue, Nov 29, 2022, 1:36 AM +00:00

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def message():
         # insert [0]["username"] at the end TRICK to remove [{'username': 'Haziq'}] and display just Haziq
         username = db.execute("select username from users where id = ?;", session["user_id"])[0]["username"]
 
-        messages = db.execute("select message from message where id ;")
+        messages = db.execute("select message from message where user_id = ?;", session["user_id"])[0]["message"]
 
         return render_template("chatroom.html", username=username)
 
