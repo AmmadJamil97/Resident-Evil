00d246720144756890b341dfea4f887faacd00a7
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-12-01 17:33:46 +0000

    Thu, Dec 1, 2022, 5:33 PM +00:00

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ db = SQL("sqlite:///finalproject.db")
 @app.route("/")
 def messages():
     MESSAGES = db.execute("SELECT user_id, message from message;")
-    username = db.execute("SELECT username from users where id IN (SELECT user_id from message);")
-    return render_template("chatroom.html", messages = MESSAGES, username=username)
+    usernames = db.execute("SELECT username from users where id IN (SELECT user_id from message);")
+    return render_template("chatroom.html", messages = MESSAGES, usernames=usernames)
 
 @app.route("/message", methods =["POST", "GET"])
 def message():
