0b28d431e52ea2128f504b914349bbef48c756e3
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-12-01 13:07:04 +0000

    Thu, Dec 1, 2022, 1:07 PM +00:00

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def default():
 def message():
 
     if request.method == "POST":
-        names = db.execute("SELECT username from users;")
+        names = db.execute("SELECT username from users;")[0]["username"]
         db.execute("INSERT INTO message (user_id, message) VALUES (?, ?);", session["user_id"], request.form.get("message"))
 
         # insert [0]["username"] at the end TRICK to remove [{'username': 'Haziq'}] and display just Haziq
