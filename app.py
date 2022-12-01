2addd25b682a8fadb9967ca49716d8e49beb92e4
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-12-01 19:42:43 +0000

    Thu, Dec 1, 2022, 7:42 PM +00:00

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def messages():
 def message():
 
     if request.method == "POST":
-
+        if 
 
         # insert [0]["username"] at the end TRICK to remove [{'username': 'Haziq'}] and display just Haziq
         # username = db.execute("select username from users where id = ?;", session["user_id"])[0]["username"]
