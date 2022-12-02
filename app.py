d561b2c5a7ded4997f57f0771416f257b25beb23
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-12-02 00:15:37 +0000

    Fri, Dec 2, 2022, 12:15 AM +00:00

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ def message():
         # create a key value pair of dicts for KEY=username, VALUE=messages, together PAIRS
         PAIRS = {'username' : ['messages']}
 
-        for id in message (0, 15):
+        max = db.execute(";")
+        for id in message (min, max):
 
 
         return redirect("/")
