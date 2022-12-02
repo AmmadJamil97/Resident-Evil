e47320ab35524421d3bfeb78728e255e5184617b
AmmadJamil97 <107445871+AmmadJamil97@users.noreply.github.com>
2022-12-02 02:26:26 +0000

    Fri, Dec 2, 2022, 2:26 AM +00:00

--- app.py
+++ app.py
@@ -27,14 +27,7 @@ def messages():
 
     usernames = db.execute("SELECT username from users;")
 
-    # MESSAGES = db.execute('SELECT * from message;')
-    # 4th try: create loop for id = 0
-
-    min_id_users = db.execute("SELECT MIN(id) FROM users;")
-    max_id_users = db.execute("SELECT MAX(id) FROM users;")
-    for x in range ( min_id_users, max_id_users):
-        MESSAGES = db.execute("SELECT * from message WHERE id = ?;", x)
-        x+=1
+    MESSAGES = db.execute('SELECT * from message;')
 
     current_user = db.execute("SELECT username from users where id = ?", session["user_id"])[0]["username"]
 
